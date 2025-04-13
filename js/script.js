0bf6b6c93b64d0d75b25a340badf0bc670c7db24
tituvai <titu17940@gmail.com>
2025-04-13 21:06:28 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,125 @@
+console.log("Titu Sarkar")
+
+
+// Var Part Start 
+
+var frist = "Hello Creative It"
+
+console.log(frist)
+
+
+var name = "Hello Creative It"
+var name = "Well Come To Creative It"
+
+console.log(name)
+
+
+var one = "Titu Sarkar"
+
+console.log(one)
+
+{
+    var tow = "Hi javaScript"
+}
+console.log(tow)
+
+var sum = 20 + 30
+console.log(sum)
+
+var maynas = 20 - 30
+console.log(maynas)
+
+var intu = 20 * 30
+
+console.log(intu)
+
+var dive = 20 / 4
+console.log(dive)
+
+
+
+// Var Part End 
+
+
+// let Part Start 
+
+let titu = "javaScript Class One"
+
+console.log(titu)
+
+let sarkar = "javaScript Two"
+ sarkar = "javaScript Three"
+
+console.log(sarkar)
+
+{
+    let uzzal = "hello javaScript"
+    console.log(uzzal)
+}
+
+{
+    let plus = 30 + 50
+     console.log(plus)
+}
+
+let plus = 20-10
+console.log(plus)
+
+let bag = 40 / 6
+console.log(bag)
+
+let gun = 10 * 20
+console.log(gun)
+
+
+// let Part End 
+
+// const Part Start 
+
+const adir = "Hello Html & Css"
+
+console.log(adir)
+
+const kazal = "Well Come To JavaScript"
+
+console.log(kazal)
+
+{
+    const kishur = "Start For Html css & javaScript"
+    console.log(kishur)
+}
+
+
+
+// const Part End 
+
+let title = "Titu Sarkar"
+let age = 21
+
+console.log("amar nam" + " " + title + "."+ " " + "amar age" + " "+ age)
+
+let namta = 2
+let final = namta + " " + "x" + " " + 1 + " " + "=" + " " + namta
+
+
+
+let two = 2 + " " + "x" + " " + 2 + " " + "=" + " " + 4
+let three = namta + " " + "x" + " " + 1 + " " + "=" + " " + 6
+let five = namta + " " + "x" + " " + 1 + " " + "=" + " " + 8
+let six = namta + " " + "x" + " " + 1 + " " + "=" + " " + 10
+let seven = namta + " " + "x" + " " + 1 + " " + "=" + " " + 12
+let eight = namta + " " + "x" + " " + 1 + " " + "=" + " " + 16
+let nine = namta + " " + "x" + " " + 1 + " " + "=" + " " + 18
+let ten = namta + " " + "x" + " " + 1 + " " + "=" + " " + 20
+
+console.log(final)
+console.log(two)
+console.log(three)
+console.log(five)
+console.log(six)
+console.log(seven)
+console.log(eight)
+console.log(nine)
+console.log(ten)
+
+
